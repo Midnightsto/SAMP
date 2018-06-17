92e58f8164df4adcb371a40eab7fd3e685292dc7
Midnightsto <39741257+Midnightsto@users.noreply.github.com>
2018-06-17 20:21:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 client.on('guildMemberAdd', member => {
-   return member.send("Welcome to the Official Nin-mmo Discord Server, please enjoy your stay. Be sure to read #rules and #general-info. For more info on the game, check #game-description. Theres also a channel for community feedback, where you can help the creation of the game by supplying feedback and ideas, called #community-feedback.");
+   return member.send("Welcome to the Official Nin-mmo Discord Server ${member} please enjoy your stay. Be sure to read #rules and #general-info. For more info on the game, check #game-description. Theres also a channel for community feedback, where you can help the creation of the game by supplying feedback and ideas, called #community-feedback.");
 });
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
