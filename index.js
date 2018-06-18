1e3b58a4188b62f08f71b34cb644945142332f9d
Midnightsto <39741257+Midnightsto@users.noreply.github.com>
2018-06-18 23:38:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Nin MMO bot V0.1`);
 });
 
 client.on("guildDelete", guild => {
