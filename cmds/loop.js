d5f6706ebbab862037e100969c622710c47176ff
Midnightsto <39741257+Midnightsto@users.noreply.github.com>
2018-07-26 00:28:16 +0300

    Create loop.js

---
+++ cmds/loop.js
@@ -0,0 +1,5 @@
+exports.run = async (client,message, args) =>{
+    const Discord = require("discord.js");
+      var interval = setInterval (function () {
+        message.channel.send("@356489931633459200")
+      }, 1 * 1000); }
