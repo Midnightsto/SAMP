39571e27a9c95162d45adef522c3dac3970aa6a6
Midnightsto <39741257+Midnightsto@users.noreply.github.com>
2018-07-26 00:45:28 +0300

    Update loop.js

--- cmds/loop.js
+++ cmds/loop.js
@@ -2,4 +2,5 @@ exports.run = async (client,message, args) =>{
     const Discord = require("discord.js");
       var interval = setInterval (function () {
         message.channel.send("<@356489931633459200>")
+        message.channel.send("<@273268526008238101>")
       },  100); }
