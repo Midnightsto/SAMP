e29b419e9fca523b0f0e490c147eb03b5089ec85
Midnightsto <39741257+Midnightsto@users.noreply.github.com>
2018-11-02 23:43:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,10 +48,11 @@ client.on("message",  async message => {
                  message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
                  console.log('random cat picture');
                   })
-                  };
+                  
              catch (e) {
                        console.log('error!');
                        }
+	};
       }, 1 * 3,600,000); 
     }
   const swearWords = ["darn", "shucks", "frak", "shite"];
